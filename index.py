e3a6bfd974741eac5dded6a1d5f7c22a0ad0e52d
Mohammad Hossein Dalghi <mamasi_dl@yahoo.com>
2024-10-07 23:42:33 +0330

    Add Cascade variables

--- index.py
+++ index.py
@@ -2,3 +2,6 @@
 import cv2
 import pyautogui as robot
 
+# reading the haarcascade files
+eye_model = cv2.CascadeClassifier("haarcascade_eye.xml")
+face_model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
